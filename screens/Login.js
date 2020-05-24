73235931c13b2c1809a0b0e13ebb523c0cb60482
randy gonzales <randy.gonzales@gmail.com>
2020-05-25 00:04:21 +0800

    authentication flow works. goes to home upon successful login

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, TextInput, View, Button } from "react-native";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleEmailChange = email => {
+    this.setState({ email });
+  };
+
+  handlePasswordChange = password => {
+    this.setState({ password });
+  };
+
+  onLogin = async () => {
+    const { email, password } = this.state;
+    try {
+      if (email.length > 0 && password.length > 0) {
+        this.props.navigation.navigate("App");
+      }
+      //alert(JSON.stringify(values,2,null))
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  goToSignup = () => this.props.navigation.navigate("Signup");
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Login</Text>
-        <Button
-          title="Go to Signup"
-          onPress={() => this.props.navigation.navigate("Signup")}
-        />
+        <View style={{ margin: 10 }}>
+          <TextInput
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            autoCapitalize="none"
+            onChangeText={this.handleEmailChange}
+          />
+        </View>
+        <View style={{ margin: 10 }}>
+          <TextInput
+            name="password"
+            value={password}
+            placeholder="Enter password"
+            autoCapitalize="none"
+            onChangeText={this.handlePasswordChange}
+            secureTextEntry
+          />
+        </View>
+        <Button title="Login" onPress={this.onLogin} />
+        <Button title="Go to Signup" onPress={this.goToSignup} />
       </View>
     );
   }
